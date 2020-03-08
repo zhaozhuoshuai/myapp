11baf74b81b70c78d877a4a456e4b6644006440f
zhuoshuai <zhuoshuai88@163.com>
2020-03-08 13:27:09 +0800

    配置less-轮播图布局

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,42 +1,53 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view>
-			<text class="title">{{title}}</text>
-		</view>
-	</view>
+  <view class="container">
+    <!-- 搜索栏 -->
+    <view class="search">
+      <view class="input-box">
+        <input type="text" />
+      </view>
+    </view>
+    <!-- 轮播图 -->
+    <swiper indicator-dots>
+      <swiper-item>
+        <image src="../../static/uploads/banner1.png" />
+      </swiper-item>
+      <swiper-item>
+        <image src="../../static/uploads/banner2.png" />
+      </swiper-item>
+      <swiper-item>
+        <image src="../../static/uploads/banner3.png" />
+      </swiper-item>
+    </swiper>
+  </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: '你好'
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
-		}
-	}
+export default {
+  data() {
+    return {
+      title: "你好"
+    };
+  },
+  onLoad() {},
+  methods: {}
+};
 </script>
 
-<style>
-	.content {
-		text-align: center;
-		height: 400upx;
+<style lang="less" scoped>
+.search {
+  .input-box {
+    background-color: #ff2d4a;
+    padding: 20rpx 16rpx;
+    input {
+      background-color: #fff;
+    }
+  }
+}
+swiper {
+	height: 340rpx;
+	image{
+		width: 750rpx;
+		height: 340rpx;
 	}
-
-	.logo {
-		height: 200upx;
-		width: 200upx;
-		margin-top: 200upx;
-	}
-
-	.title {
-		font-size: 36upx;
-		color: #8f8f94;
-	}
+}
 </style>
