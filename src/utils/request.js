ac5c47222c07723abe580830a39c0be4987905ff
zhuoshuai <zhuoshuai88@163.com>
2020-03-09 20:59:58 +0800

    加载提示

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,11 +1,15 @@
 export default {
     install: (vue, options) => {
-        vue.prototype.$request = async({path}) => {
+        vue.prototype.$request = async ({ path }) => {
             //实现发送请求
-            const [error, res] = await uni.request({
-                url:options.baseURL+path
+            uni.showLoading({
+                title: '正在加载数据...'
             })
-            
+            const [error, res] = await uni.request({
+                url: options.baseURL + path
+            })
+            // 隐藏提示
+            uni.hideLoading()
             //返回异步接口的结果
             return res.data
         }
